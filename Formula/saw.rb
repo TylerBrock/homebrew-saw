62a4eb0b634af4246c2c8914f9518062c2d50369
TylerBrock <tyler.brock@gmail.com>
2018-05-28 19:52:13 -0700

    Brew formula update for saw version v0.0.5

--- Formula/saw.rb
+++ Formula/saw.rb
@@ -4,7 +4,7 @@ class Saw < Formula
   url "https://github.com/TylerBrock/saw/archive/v0.0.5.tar.gz"
   head "https://github.com/TylerBrock/saw.git"
   version "0.0.5"
-  sha256 "4c35d938a93179a40f09ec6fd3a4e133f79359c692ad5d66defd171f475223a2"
+  sha256 "90aebe4e2a8da07dce9c95d3ef0780f9946566efec19c44c119d0882776e4e17"
   depends_on "go" => :build
   depends_on "dep" => :build
 
