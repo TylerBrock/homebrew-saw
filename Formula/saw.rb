8f2075cab23f1bf3d0e5fb25616b2c5e50e592ab
TylerBrock <tyler.brock@gmail.com>
2018-05-29 20:55:50 -0700

    Brew formula update for saw version v0.1.2

--- Formula/saw.rb
+++ Formula/saw.rb
@@ -3,7 +3,7 @@ class Saw < Formula
   homepage "https://github.com/TylerBrock/saw"
   url "https://github.com/TylerBrock/saw/releases/download/v0.1.2/saw_0.1.2_darwin_amd64.tar.gz"
   version "0.1.2"
-  sha256 "3debb2dafa57f338be01df6cff1cc2ded6f8c77a34937e6cecbf16a7317ae7a5"
+  sha256 "d4c241a5b8436ec8326baf6a5d722bae04d8081bf56b0e0e54639c1b6058bb67"
 
   def install
     bin.install "saw"
