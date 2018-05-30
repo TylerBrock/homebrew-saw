bf44e5b1706495f4f79dbc70df5de4a25da0b40d
TylerBrock <tyler.brock@gmail.com>
2018-05-29 18:36:25 -0700

    Brew formula update for saw version v0.1.2

--- Formula/saw.rb
+++ Formula/saw.rb
@@ -1,12 +1,9 @@
 class Saw < Formula
   desc "Fast, multipurpose tool for AWS CloudWatch Logs"
   homepage "https://github.com/TylerBrock/saw"
-  url "https://github.com/TylerBrock/saw/archive/v0.1.2.tar.gz"
-  head "https://github.com/TylerBrock/saw.git"
+  url "https://github.com/TylerBrock/saw/releases/download/v0.1.2/saw_0.1.2_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
   version "0.1.2"
-  sha256 "f7398ea54c80befe994f898010c21ef9a62c880c2f25a9ac2bf84765a09b5313"
-  depends_on "go" => :build
-  depends_on "dep" => :build
+  sha256 "b89b7c4ba08674b80f9bb5a9ac734f209fa9a2451bcc6a41a9d3c5bbb6d9f7bb"
 
   def install
     bin.install "saw"
